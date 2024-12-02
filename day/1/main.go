9e65a8d8c63a128516e649fd6eea7861a3627044
akeril <185402721+akeril@users.noreply.github.com>
2024-12-03 00:41:12 +0530

    refactor day 1 code

--- day/1/main.go
+++ day/1/main.go
@@ -1,61 +1,49 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"sort"
-	"strconv"
+	"slices"
 	"strings"
+
+	utl "github.com/kjabin/aoc2024/utils"
 )
 
-func Input(file string) ([]int, []int) {
-	f, _ := os.Open(file)
-	sc := bufio.NewScanner(f)
-
-	l1 := make([]int, 0)
-	l2 := make([]int, 0)
-	for sc.Scan() {
-		locations := strings.Split(sc.Text(), "   ")
-		v1, _ := strconv.Atoi(locations[0])
-		v2, _ := strconv.Atoi(locations[1])
-		l1 = append(l1, v1)
-		l2 = append(l2, v2)
-	}
-	return l1, l2
+func main() {
+	lines := utl.ReadFile("input")
+	lloc, rloc := ParseInput(lines)
+	fmt.Println(CalcDiff(lloc, rloc))
+	fmt.Println(CalcSim(lloc, rloc))
 }
 
-func Part1(l1, l2 []int) int {
-	sort.Slice(l1, func(i, j int) bool { return l1[i] < l1[j] })
-	sort.Slice(l2, func(i, j int) bool { return l2[i] < l2[j] })
+func ParseInput(lines []string) ([]int, []int) {
+	var lloc, rloc []int
+	for _, line := range lines {
+		nums := strings.Split(line, "   ")
+		l, r := utl.ToInt(nums[0]), utl.ToInt(nums[1])
+		lloc = append(lloc, l)
+		rloc = append(rloc, r)
+	}
+	return lloc, rloc
+}
 
+func CalcDiff(lloc, rloc []int) int {
+	slices.Sort(lloc)
+	slices.Sort(rloc)
 	diff := 0
-	for i := 0; i < len(l1); i++ {
-		if l1[i] < l2[i] {
-			diff += l2[i] - l1[i]
-		} else {
-			diff += l1[i] - l2[i]
-		}
+	for i := 0; i < len(lloc); i++ {
+		diff += utl.Abs(lloc[i] - rloc[i])
 	}
 	return diff
 }
 
-func Part2(l1, l2 []int) int {
+func CalcSim(lloc, rloc []int) int {
 	counter := make(map[int]int)
-	for _, x := range l2 {
-		counter[x] += 1
+	for _, num := range rloc {
+		counter[num] += 1
 	}
 	sim := 0
-	for _, x := range l1 {
-		if y, ok := counter[x]; ok {
-			sim += x * y
-		}
+	for _, num := range lloc {
+		sim += num * counter[num]
 	}
 	return sim
-}
-
-func main() {
-	l1, l2 := Input("input")
-	// fmt.Println(Part1(l1, l2))
-	fmt.Println(Part2(l1, l2))
 }
