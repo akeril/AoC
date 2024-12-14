491b08a80359b1a40f05a65869790fd08b0a582b
akeril <185402721+akeril@users.noreply.github.com>
2024-12-15 02:23:31 +0530

    day 14 part 2

--- day/14/main.go
+++ day/14/main.go
@@ -20,29 +20,73 @@ func main() {
 			utils.ToInt(b[2]),
 		})
 	}
-	t := 100
+	t := 0
 	h, w := 103, 101
-	hm, wm := (h)/2, (w)/2
-	q1, q2, q3, q4 := 0, 0, 0, 0
-	for _, bot := range bots {
-		px := mod(bot.x+t*bot.vx, h)
-		py := mod(bot.y+t*bot.vy, w)
-
-		if (px < hm) && (py < wm) {
-			q1++
+	for {
+		matrix := Matrix(h, w)
+		for _, bot := range bots {
+			px := mod(bot.x+t*bot.vx, h)
+			py := mod(bot.y+t*bot.vy, w)
+			matrix[px][py]++
 		}
-		if (px < hm) && (py > wm) {
-			q2++
+		fmt.Println(t)
+		if largestComponent(matrix) > 100 {
+			Print(matrix)
+			var z string
+			fmt.Scanln(&z)
 		}
-		if (px > hm) && (py < wm) {
-			q3++
-		}
-		if (px > hm) && (py > wm) {
-			q4++
-		}
-		fmt.Println(bot, px, py)
+		t++
 	}
-	fmt.Println(q1, q2, q3, q4, q1*q2*q3*q4)
+}
+
+func largestComponent(matrix [][]int) int {
+	largest := 0
+	visited := make(map[move]bool)
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			largest = max(largest, dfs(matrix, i, j, visited))
+		}
+	}
+	return largest
+}
+
+type move struct {
+	x, y int
+}
+
+func dfs(matrix [][]int, i, j int, visited map[move]bool) int {
+	if !inBound(matrix, i, j) {
+		return 0
+	}
+	if matrix[i][j] == 0 {
+		return 0
+	}
+	if visited[move{i, j}] {
+		return 0
+	}
+	visited[move{i, j}] = true
+	count := 1
+	moves := []move{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+	for _, move := range moves {
+		count += dfs(matrix, i+move.x, j+move.y, visited)
+	}
+	return count
+}
+
+func inBound(matrix [][]int, i, j int) bool {
+	return i >= 0 && i < len(matrix) && j >= 0 && j < len(matrix[0])
+}
+func Print(matrix [][]int) {
+	for _, row := range matrix {
+		for _, c := range row {
+			if c == 0 {
+				fmt.Printf("  ")
+			} else {
+				fmt.Printf("# ")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 type Bot struct {
@@ -51,4 +95,16 @@ type Bot struct {
 
 func mod(x, y int) int {
 	return (x%y + y) % y
+}
+
+func Matrix(h, w int) [][]int {
+	var matrix [][]int
+	for i := 0; i < h; i++ {
+		var row []int
+		for j := 0; j < w; j++ {
+			row = append(row, 0)
+		}
+		matrix = append(matrix, row)
+	}
+	return matrix
 }
