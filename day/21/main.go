95d8c87dfc5223d4bf86066a1a0abf64292a04a6
akeril <185402721+akeril@users.noreply.github.com>
2025-01-03 21:59:07 +0530

    day 21, part 2

--- day/21/main.go
+++ day/21/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
+	"strings"
 
 	"github.com/kjabin/aoc2024/utils"
 )
@@ -10,103 +12,133 @@ import (
 func main() {
 	input := utils.ReadFile("input")
 	keypad := []string{"789", "456", "123", "X0A"}
-	touchpad := []string{"X^A", "<v>"}
+	directionalpad := []string{"X^A", "<v>"}
+	directionalseq := seq(directionalpad)
+	cache := make(map[citem]int)
 	total := 0
 	for _, code := range input {
-		n := len(code)
-		plen := solve(keypad, touchpad, code)
-		num, _ := strconv.Atoi(code[:n-1])
-		total += num * plen
+		mi := math.MaxInt
+		for _, path := range search(keypad, code) {
+			c := 0
+			for _, p := range zip("A" + path) {
+				c += cost(cache, directionalseq, p.s, p.e, 25)
+			}
+			mi = min(mi, c)
+		}
+		num, _ := strconv.Atoi(code[:len(code)-1])
+		total += num * mi
 	}
 	fmt.Println(total)
 }
 
-func solve(keypad, touchpad []string, code string) int {
-	p1 := translate(keypad, code)
-
-	p2 := make([]string, 0)
-	for _, p := range p1 {
-		t := translate(touchpad, p)
-		p2 = append(p2, t...)
-	}
-
-	p3 := make([]string, 0)
-	for _, p := range p2 {
-		t := translate(touchpad, p)
-		p3 = append(p3, t...)
-	}
-
-	mx := 1_000_000
-	for _, p := range p3 {
-		mx = min(mx, len(p))
-	}
-	return mx
+type citem struct {
+	p pair
+	d int
 }
 
-func translate(pad []string, code string) []string {
-	p := find(pad, 'A')
+type pair struct {
+	s, e rune
+}
+
+func cost(cache map[citem]int, seq map[pair][]string, start, end rune, depth int) int {
+	p := pair{start, end}
+	ci := citem{p, depth}
+	if depth == 1 {
+		return minlenstr(seq[p])
+	}
+	if v, ok := cache[ci]; ok {
+		return v
+	}
+	mi := math.MaxInt
+	for _, path := range seq[p] {
+		c := 0
+		for _, pp := range zip("A" + path) {
+			c += cost(cache, seq, pp.s, pp.e, depth-1)
+		}
+		mi = min(mi, c)
+	}
+	cache[ci] = mi
+	return mi
+}
+
+func zip(s string) []pair {
+	var res []pair
+	for i := 1; i < len(s); i++ {
+		res = append(res, pair{rune(s[i-1]), rune(s[i])})
+	}
+	return res
+}
+
+func seq(pad []string) map[pair][]string {
+	mp := make(map[pair][]string)
+	s := strings.Join(pad, "")
+	for _, start := range s {
+		for _, end := range s {
+			mp[pair{start, end}] = bfs(pad, start, end)
+		}
+	}
+	return mp
+}
+
+func search(pad []string, code string) []string {
 	paths := []string{""}
-	for _, c := range code {
-		paths = Product(paths, search(pad, p, find(pad, c)))
-		p = find(pad, c)
+	for _, p := range zip("A" + code) {
+		paths = product(paths, bfs(pad, p.s, p.e))
 	}
 	return paths
 }
 
-func Product(p, q []string) []string {
-	res := make([]string, 0, len(p)*len(q))
-	for _, s := range p {
-		for _, t := range q {
+func product(ss []string, tt []string) []string {
+	var res []string
+	for _, s := range ss {
+		for _, t := range tt {
 			res = append(res, s+t)
 		}
 	}
 	return res
 }
 
-func search(pad []string, p, q Pos) []string {
-	queue := []Item{{p, ""}}
-	visited := make(map[Pos]bool)
-	moves := []Move{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+// find all shortest path between two points
+func bfs(pad []string, start, end rune) []string {
+	if start == 'X' || end == 'X' {
+		return []string{}
+	}
+	type item struct {
+		p    pos
+		path string
+	}
+	maxlen := math.MaxInt
 	paths := make([]string, 0)
-	mx := 1_000_000_000
+	queue := []item{{find(pad, start), ""}}
+	moves := []move{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
 	for len(queue) > 0 {
 		p, path := queue[0].p, queue[0].path
 		queue = queue[1:]
-		if !bound(pad, p) || visited[p] || pad[p.x][p.y] == 'X' {
+		if !bound(pad, p) || pad[p.x][p.y] == 'X' {
 			continue
 		}
-		if len(path) > mx {
+		if len(path) > maxlen {
 			continue
 		}
-		if p.x == q.x && p.y == q.y {
-			mx = min(len(path), mx)
-			paths = append(paths, path+"A")
+		if pad[p.x][p.y] == byte(end) {
+			path = path + "A"
+			maxlen = len(path)
+			paths = append(paths, path)
 			continue
 		}
 		for _, m := range moves {
-			pnew := Pos{p.x + m.dx, p.y + m.dy}
-			queue = append(queue, Item{pnew, path + m.dir()})
+			it := item{pos{p.x + m.dx, p.y + m.dy}, path + m.dir()}
+			queue = append(queue, it)
 		}
 	}
 	return paths
 }
 
-type Move struct {
+type move struct {
 	dx, dy int
 }
 
-func find(pad []string, s rune) Pos {
-	for i := 0; i < len(pad); i++ {
-		for j := 0; j < len(pad[0]); j++ {
-			if pad[i][j] == byte(s) {
-				return Pos{i, j}
-			}
-		}
-	}
-	return Pos{}
-}
-
-func (m Move) dir() string {
+func (m move) dir() string {
 	switch {
 	case m.dx == 1 && m.dy == 0:
 		return "v"
@@ -116,20 +148,33 @@ func (m Move) dir() string {
 		return ">"
 	case m.dx == 0 && m.dy == -1:
 		return "<"
-	default:
-		panic("???")
 	}
+	return "?"
 }
 
-type Item struct {
-	p    Pos
-	path string
-}
-
-type Pos struct {
+type pos struct {
 	x, y int
 }
 
-func bound(matrix []string, p Pos) bool {
-	return p.x >= 0 && p.x < len(matrix) && p.y >= 0 && p.y < len(matrix[0])
+func bound(pad []string, p pos) bool {
+	return p.x >= 0 && p.x < len(pad) && p.y >= 0 && p.y < len(pad[0])
+}
+
+func find(pad []string, c rune) pos {
+	for i, row := range pad {
+		for j, cc := range row {
+			if c == cc {
+				return pos{i, j}
+			}
+		}
+	}
+	return pos{}
+}
+
+func minlenstr(ss []string) int {
+	mi := math.MaxInt
+	for _, s := range ss {
+		mi = min(mi, len(s))
+	}
+	return mi
 }
