7f19e1fd07a527f68bd91e1e1ae04c290d67c1d7
akeril <185402721+akeril@users.noreply.github.com>
2025-01-04 00:43:54 +0530

    day 24, part 2

--- day/24/main.go
+++ day/24/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"slices"
 	"strconv"
+	"strings"
 
 	"github.com/kjabin/aoc2024/utils"
 )
@@ -27,14 +28,39 @@ func main() {
 		formulas[w3] = Formula{w1, w2, op}
 	}
 
+	formulas["z11"], formulas["wpd"] = formulas["wpd"], formulas["z11"]
+	formulas["jqf"], formulas["skh"] = formulas["skh"], formulas["jqf"]
+	formulas["z19"], formulas["mdd"] = formulas["mdd"], formulas["z19"]
+	formulas["z37"], formulas["wts"] = formulas["wts"], formulas["z37"]
+
 	for w := range formulas {
 		values[w] = calc(values, formulas, w)
 	}
 
-	fmt.Println(reg(values, 'z'))
+	zbits := reg(values, 'z')
+	xbits := reg(values, 'x')
+	ybits := reg(values, 'y')
+
+	fmt.Printf("0%s\n", xbits)
+	fmt.Printf("0%s\n", ybits)
+	fmt.Println(zbits)
 }
 
-func reg(values map[string]int, c byte) int64 {
+func pprint(values map[string]int, formulas map[string]Formula, w string, depth int) {
+	if depth > 4 {
+		return
+	}
+	if _, ok := formulas[w]; ok {
+		f := formulas[w]
+		fmt.Println(strings.Repeat(" ", depth), f.op, w)
+		pprint(values, formulas, f.w1, depth+1)
+		pprint(values, formulas, f.w2, depth+1)
+	} else {
+		fmt.Println(strings.Repeat(" ", depth), w)
+	}
+}
+
+func reg(values map[string]int, c byte) string {
 	i := 0
 	bits := ""
 	for {
@@ -45,8 +71,7 @@ func reg(values map[string]int, c byte) int64 {
 		bits = strconv.Itoa(values[reg]) + bits
 		i += 1
 	}
-	num, _ := strconv.ParseInt(bits, 2, 64)
-	return num
+	return bits
 }
 
 func calc(values map[string]int, formulas map[string]Formula, w string) int {
